b00be4524f2ca2ff889d2b266d9b01126cde1029
Mohammad Alseidi <mfaraj981@gmail.com>
2026-04-24 17:44:59 +0300

    Fix: Verified log

--- sanity.config.js
+++ sanity.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'sanity';
+import { defineConfig, buildLegacyTheme } from 'sanity';
 import { structureTool } from 'sanity/structure';
 import { visionTool } from '@sanity/vision';
 import vehicle from './schemas/vehicle';
@@ -6,9 +6,52 @@ import brand from './schemas/brand';
 import category from './schemas/category';
 import siteSettings from './schemas/siteSettings';
 
+// Custom Theme Colors (Laval Luxury Motors Brand)
+const props = {
+  '--laval-white': '#fff',
+  '--laval-black': '#000',
+  '--laval-gold': '#C5A059',
+  '--laval-grey': '#151515',
+};
+
+export const myTheme = buildLegacyTheme({
+  /* Base theme colors */
+  '--black': props['--laval-black'],
+  '--white': props['--laval-white'],
+
+  '--gray': '#666',
+  '--gray-base': '#666',
+
+  '--component-bg': props['--laval-grey'],
+  '--component-text-color': props['--laval-white'],
+
+  /* Brand */
+  '--brand-primary': props['--laval-gold'],
+
+  /* Default button */
+  '--default-button-color': '#666',
+  '--default-button-primary-color': props['--laval-gold'],
+  '--default-button-success-color': '#0f9d58',
+  '--default-button-warning-color': '#f4b400',
+  '--default-button-danger-color': '#db4437',
+
+  /* State */
+  '--state-info-color': props['--laval-gold'],
+  '--state-success-color': '#0f9d58',
+  '--state-warning-color': '#f4b400',
+  '--state-danger-color': '#db4437',
+
+  /* Navbar */
+  '--main-navigation-color': props['--laval-black'],
+  '--main-navigation-color--inverted': props['--laval-white'],
+
+  '--focus-color': props['--laval-gold'],
+});
+
 export default defineConfig({
   name: 'default',
   title: 'Laval Luxury Motors | Admin',
+  theme: myTheme, // Apply the custom gold theme
 
   projectId: 'nyiie8qw', 
   dataset: 'production',
